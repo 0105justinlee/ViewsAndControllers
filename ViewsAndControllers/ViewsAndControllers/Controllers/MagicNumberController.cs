8641c2d9b7a71ef50ae525c3dfd59dd5e9d820b8
Justin <0105justinlee@gmail.com>
2019-06-19 11:18:00 -0500

    training stuff

---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/MagicNumberController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class MagicNumberController : Controller
+    {
+        // GET: MagicNumber
+        public ActionResult Index()
+        {
+            return View(viewName: "index", model: 0);
+        }
+
+        [HttpPost]
+        public ActionResult MagicNumber(int inputNumber1, int inputNumber2)
+        {
+            Random random = new Random();
+            int random1;
+            int random2;
+            if (inputNumber1 > inputNumber2)
+            {
+                random1 = inputNumber2;
+                random2 = inputNumber1;
+            }
+            else
+            {
+                random1 = inputNumber1;
+                random2 = inputNumber2;
+            }
+            return View(viewName: "Index", model: random.Next(random1, random2 + 1));
+        }
+    }
+}
