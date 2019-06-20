39949e91b503f6b6ff3db4d44865ee11252156fe
Justin <0105justinlee@gmail.com>
2019-06-19 23:00:30 -0500

    V&C Extension Activities

---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/HowOldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class HowOldController : Controller
+    {
+        // GET: HowOld
+        public ActionResult Index()
+        {
+            
+            return View(viewName: "Index", model: 0);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime inputDate)
+        {
+            TimeSpan distance = DateTime.Now - inputDate;
+            int year = (int) distance.TotalDays / 365;
+            return View(viewName: "Index", model: year);
+        }
+    }
+}
---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/LastNameAgeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class LastNameAgeController : Controller
+    {
+        // GET: LastNameAge
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: 0);
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string lastName, int age)
+        {
+            return View(viewName: "Index", model: Math.Abs(lastName.Length - age));
+        }
+    }
+}
